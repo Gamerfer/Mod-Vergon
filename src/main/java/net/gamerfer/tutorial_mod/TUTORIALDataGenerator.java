97bc201a1e4acdc3b34285f445ba8e4c6579221c
gamerfer <juanfernandolopezsalaz@gmail.com>
2025-06-25 19:02:21 -0600

    zxccZX

---
+++ src/main/java/net/gamerfer/tutorial_mod/TUTORIALDataGenerator.java
@@ -0,0 +1,11 @@
+package net.gamerfer.tutorial_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TUTORIALDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
